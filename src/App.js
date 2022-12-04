efcdd96339e7bd3a8434455123481fa05cbf2a1d
Saurabh Patel <saurabh@upsitez.com>
2022-12-04 12:39:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecommerce App</h1>
+      <button>HTML Button</button>
+      <Button>React Component Button</Button>
+    </div>
+  );
+}
+
+export default App;
